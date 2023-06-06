43644f3dd8d0cc98c5885fd053ea96913e0241de
Your Name <retyunskikh2003@gmail.com>
2023-06-06 10:21:13 +0300

    Initial commit

---
+++ Lab2/Lab2/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+namespace Lab2;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCompatibility()
+            .ConfigureMauiHandlers((handlers) => {
+#if ANDROID
+			handlers.AddHandler(typeof(Lab2.Controls.BorderedEntry),typeof(Lab2.Platforms.Android.Renderers.BorderedEntryRenderer));
+#endif
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+		return builder.Build();
+	}
+}
+
+
